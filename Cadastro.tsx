f32208959158a89a6dc343cac1794879eac192c7
IsaqueMGG <100934859+IsaqueMGG@users.noreply.github.com>
2023-06-07 11:15:43 -0300

    Create Cadastro.tsx

---
+++ Cadastro.tsx
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Cadastro</Text>
+      <TextInput placeholder='Nome'/>
+      <TextInput placeholder='Email'/>
+      <TextInput placeholder='Data de nascimento'/>
+      <TextInput placeholder='CPF'/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titulo:{
+    fontSize: 26,
+    fontWeight: 'bold',
+  }
+});
